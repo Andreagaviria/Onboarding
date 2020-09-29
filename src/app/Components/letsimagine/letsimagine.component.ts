279c732dd272bc5a231d5e8b85730f71e60bdfd2
Andrea Gaviria <angapa46@outlook.com>
2020-09-28 19:02:06 -0500

    add text and styles

---
+++ src/app/Components/letsimagine/letsimagine.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-letsimagine',
+  templateUrl: './letsimagine.component.html',
+  styleUrls: ['./letsimagine.component.css']
+})
+export class LetsimagineComponent implements OnInit {
+ nameUser:string="ana";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { InputUserNameComponent } from './Components/input-user-name/input-user-name.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LetsimagineComponent } from './components/letsimagine/letsimagine.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputUserNameComponent
+    InputUserNameComponent,
+    LetsimagineComponent
   ],
   imports: [
-    BrowserModule, FormsModule, ReactiveFormsModule
+    BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
